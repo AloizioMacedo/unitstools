3d1fc9fa2913812e7d6a7a7f205ec9417f67bae6
Aloizio Macedo <aloiziomacedo@gmail.com>
2022-12-25 01:43:51 -0300

    Add docstrings

--- units/units.py
+++ units/units.py
@@ -3,6 +3,8 @@ from typing import Callable, Self, Type, TypeVar, overload
 
 
 class Unit(ABC):
+    """Inherit from this class to create units (e.g., Sec or Min)."""
+
     @abstractmethod
     def __add__(self, other: Self) -> Self:
         ...
@@ -41,6 +43,8 @@ class Unit(ABC):
 
 
 class IntUnit(ABC):
+    """Inherit from this class to create units that take integer values."""
+
     @abstractmethod
     def __add__(self, other: Self) -> Self:
         ...
@@ -88,6 +92,15 @@ _IU = TypeVar("_IU", bound=IntUnit)
 def create_conversion(
     type1: Type[_T1], type2: Type[_T2], rate: int | float
 ) -> Callable[[_T1], _T2]:
+    """Creates a conversion function for the given types.
+
+    :param type1: Given class inherited from Unit or IUnit to convert from.
+    :type type1: Type[Unit] | Type[IUnit].
+    :param type2: Given class inherited from Unit or IUnit to convert to.
+    :type type2: Type[Unit] | Type[IUnit].
+    :param rate: Multiplier factor for the conversion, i.e. type1 = 60*type2
+    :type rate: int, float."""
+
     def conversion(x: _T1) -> _T2:
         return x * rate  # type: ignore
 
@@ -105,6 +118,10 @@ def strip_units(x: Unit) -> int | float:
 
 
 def strip_units(x):
+    """Strips the unit of the given Unit or IUnit.
+
+    :param x: Specified value.
+    :type x: Unit | IUnit."""
     return x  # type: ignore
 
 
@@ -119,4 +136,10 @@ def embed_unit(x: float, unit: Type[_U]) -> _U:
 
 
 def embed_unit(x, unit):
+    """Sets the given value to a specified unit.
+
+    :param x: Specified value.
+    :type x: int | float.
+    :param unit: Given class inherited from Unit or IUnit.
+    :type unit: Type[Unit] | Type[IUnit]."""
     return x  # type: ignore
