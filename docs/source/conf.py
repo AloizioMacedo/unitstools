7146f575081bbda1e7c450613aab226d528361de
Aloizio Macedo <aloiziomacedo@gmail.com>
2022-12-25 13:54:51 -0300

    Use numpy-styled docstrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ release = "0.1"
 
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
 ]
 
 templates_path = ["_templates"]
--- unitstools/unitstools.py
+++ unitstools/unitstools.py
@@ -94,14 +94,20 @@ def create_conversion(
 ) -> Callable[[_T1], _T2]:
     """Creates a conversion function for the given types.
 
-    :param type1: Given class inherited from Unit or IUnit to convert from.
-    :type type1: Type[Unit] | Type[IntUnit]
-    :param type2: Given class inherited from Unit or IUnit to convert to.
-    :type type2: Type[Unit] | Type[IntUnit]
-    :param rate: Multiplier factor for the conversion, i.e. type1 = 60*type2
-    :type rate: int, float
-    :return: Function converting one unit to another.
-    :rtype: Callable[T1, T2]"""
+    Parameters
+    ----------
+    type1 : Type[_T1]
+        Given class inherited from Unit or IUnit to convert from.
+    type2 : Type[_T2]
+        Given class inherited from Unit or IUnit to convert to.
+    rate : int | float
+        Multiplier factor for the conversion, i.e. type1 = 60*type2
+
+    Returns
+    -------
+    Callable[[_T1], _T2]
+        Function converting one unit to another.
+    """
 
     def conversion(x: _T1) -> _T2:
         return x * rate  # type: ignore
@@ -120,12 +126,18 @@ def strip_unit(x: Unit) -> int | float:
 
 
 def strip_unit(x):
-    """Strips the unit of the given Unit or IUnit.
+    """Strips the unit of the given Unit or IntUnit.
 
-    :param x: Specified value.
-    :type x: Unit | IntUnit
-    :return: Float or int representing the value.
-    :rtype: int | float"""
+    Parameters
+    ----------
+    x : Specified value.
+        Unit | IntUnit
+
+    Returns
+    -------
+    int | float
+        Float or int representing the value.
+    """
     return x  # type: ignore
 
 
@@ -142,20 +154,32 @@ def embed_unit(x: float, unit: Type[_U]) -> _U:
 def embed_unit(x, unit):
     """Sets the given value to a specified unit.
 
-    :param x: Specified value.
-    :type x: int | float
-    :param unit: Given class inherited from Unit or IUnit.
-    :type unit: Type[Unit] | Type[IntUnit]
-    :return: Unit-imbued value.
-    :rtype: Unit | IntUnit"""
+    Parameters
+    ----------
+    x : int | float
+        Specified value.
+    unit : Type[Unit] | Type[IntUnit]
+        Given class inherited from Unit or IntUnit.
+
+    Returns
+    -------
+    Unit | IntUnit
+        Unit-imbued value.
+    """
     return x  # type: ignore
 
 
 def sum_iter(values: Iterable[_T1]) -> _T1:
     """Calculates sum of unit values in an iterable.
 
-    :param values: Values which bear an unit.
-    :type values: list
-    :return: Sum of the values.
-    :rtype: Type[Unit] | Type[IntUnit]"""
+    Parameters
+    ----------
+    values : list
+        Values which bear an unit.
+
+    Returns
+    -------
+    Type[Unit] | Type[IntUnit]
+        Sum of the values.
+    """
     return sum(values)  # type: ignore
